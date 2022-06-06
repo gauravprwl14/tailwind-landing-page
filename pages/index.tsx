036ae004062d8fb457a43bb1b7e55349801c44e0
Gaurav Porwal <gauravporwal@Gaurav-Porwal-B154s-MacBook-Pro.local>
2022-06-06 17:15:03 +0530

    fix the header styling issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ const Home: NextPage = () => {
           title={title}
           description={description}
           bgContainer="bg-center"
-          headerContainer="justify-center"
+          headerContainer="mt-28"
         />
       </div>
 
