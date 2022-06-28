c253c5420bed485cd1c6365882cfc92bdca94660
Gaurav Porwal <gauravprwl14@gmail.com>
2022-06-28 14:08:48 +0530

    fixed the header styling and update the content for the service section

--- components/Home/ServiceSection.tsx
+++ components/Home/ServiceSection.tsx
@@ -29,17 +29,17 @@ const CardJSON = [
 
     },
     {
-        title: 'Cloud-Native Software',
-        description: 'Build beautiful software that perfectly solves your business challenges.',
+        title: 'Cloud Migration',
+        description: 'Move your legacy systems to the cloud and increase performance, reliability, availability, and scalability.',
         mainFeatures: [
             {
-                text: 'Beautiful cloud-native applications for mobile, web, and desktop.'
+                text: 'Migrate legacy systems, as-is, to the clouds..'
             },
             {
-                text: 'Rebuild legacy applications to be cloud-native.'
+                text: 'Rebuild existing systems using cloud-native architecture & standards.'
             },
             {
-                text: 'Enterprise-level backend software for cloud-native services. '
+                text: 'Move stand-alone workloads to the cloud.'
             },
         ]
 
