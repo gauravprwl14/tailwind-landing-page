82695719543f44604cd34f6943d4a73e7c3cea2e
Gaurav Porwal <gauravprwl14@gmail.com>
2022-06-28 06:02:20 +0530

    setup the next js for static build generation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        loader: 'akamai',
+        path: '/',
+    },
 }
