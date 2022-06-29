c8388217e79ecc966e7c42538673a2f3e81fbc09
Gaurav Porwal <gauravprwl14@gmail.com>
2022-06-29 17:55:18 +0530

    removed the unwated code from the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -47,64 +47,9 @@ const Footer = () => {
     )
 }
 
-const FooterV2 = () => {
-    return (
-        <footer className="flex w-full items-center justify-center bg-[url('/img/footer_bg.svg')]  bg-no-repeat bg-cover dotted-border-green">
-            <div className=''>
-                <div className='grid md:grid-cols-3 gap-8 pt-8 md:pt-12 auto-rows-fr'>
-                    <div className='flex flex-col justify-around text-white md:col-span-2'>
-                        <NavigationLinks />
-                    </div>
-                    <div className='flex flex-col justify-center text-white md:col-span-1 md:row-span-2 '>
-                        <ActionButton />
-                    </div>
-                    <div className='flex flex-col justify-around text-white md:col-span-2'>
-                        <CopyRightText />
-                    </div>
-                </div>
 
 
 
 
-
-
-
-                {/* <a
-                className="flex items-center justify-center gap-2"
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by{' '}
-                <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-            </a> */}
-            </div>
-        </footer>
-    )
-}
-
-
-
-
-
-// const FooterV1 = () => {
-//     return (
-//         <footer className="flex w-full items-center justify-center bg-[url('/img/footer_bg.svg')] bg-no-repeat bg-cover">
-//             <div className='max-w-6xl mx-auto px-4 sm:px-6'>
-//                 <div className='grid sm:grid-cols-2 gap-8 pt-8 md:pt-12 '>
-//                     <div className='flex flex-col justify-around text-white'>
-//                         <NavigationLinks />
-//                     </div>
-//                     <div className='flex flex-col justify-around text-white'>
-//                         <ActionButton />
-//                     </div>
-//                     <div className='flex flex-col justify-around text-white'>
-//                         <CopyRightText />
-//                     </div>
-//                 </div>
-//             </div>
-//         </footer>
-//     )
-// }
 
 export default Footer
