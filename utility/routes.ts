8c6b6b6cbeefd72ac8ce6caf2d2fbd94ccb50f0d
Gaurav Porwal <gauravprwl14@gmail.com>
2022-07-02 22:49:31 +0530

    change the animation timing from 3s to 2s and added the conditional .html extension for routes

--- utility/routes.ts
+++ utility/routes.ts
@@ -3,8 +3,20 @@
 console.log('%c process env ', 'background: lime; color: black', { env: process.env });
 console.log('%c process env production', 'background: lime; color: black', { env: process.env.production });
 
-export const ROUTES = {
+export let ROUTES = {
     'home': '/',
     'aboutUs': '/about-us.html',
     'services': '/services.html'
 }
+
+if (process.env.NODE_ENV === 'development') {
+    ROUTES = {
+        'home': '/',
+        'aboutUs': '/about-us',
+        'services': '/services'
+    }
+}
+
+
+
+export default ROUTES
