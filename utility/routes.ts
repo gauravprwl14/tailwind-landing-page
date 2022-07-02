e0a338e8c50fff66b83052062a3b7346f3b56fa1
Gaurav Porwal <gauravprwl14@gmail.com>
2022-07-02 20:36:13 +0530

    added the html extension

--- utility/routes.ts
+++ utility/routes.ts
@@ -1,5 +1,9 @@
+
+
+console.log('%c process env ', 'background: lime; color: black', { env: process.env });
+
 export const ROUTES = {
     'home': '/',
-    'aboutUs': '/about-us',
-    'services': '/services'
+    'aboutUs': '/about-us.html',
+    'services': '/services.html'
 }
