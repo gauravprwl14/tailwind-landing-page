0b2250fff997307f5b11d757c1701e8e2f34a546
Gaurav Porwal <gauravprwl14@gmail.com>
2022-07-23 18:12:44 +0530

    enhance the animation: now the element slide left and right from the center in the what-we-do page

--- components/Services/ServiceDetails.tsx
+++ components/Services/ServiceDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image'
 import Button from '../button'
 import { ServiceType, ServicePointersType, ServiceImageType } from '../../types/commonTypes'
@@ -7,16 +7,17 @@ type ImageContainerPropTypes = {
 }
 
 
+const isServer = typeof window === 'undefined'
+const WOW = !isServer ? require('wowjs') : null
 
 
+console.log('%c WOW ', 'background: lime; color: black', { WOW });
 
-const ImageContainer = ({ obj }: { obj: ServiceImageType }) => {
+
+const ImageContainer = ({ obj, position }: { obj: ServiceImageType, position: string }) => {
     return (
-        <div className='flex flex-1 md:pr-4 mb-4 md:mb-0'>
+        <div className={`flex flex-1 md:pr-4 mb-4 md:mb-0 wow animate__animated ${animationClass(position)}`}>
             <div className={`w-full h-full flex items-center justify-center ${obj.classes} bg-contain bg-center bg-no-repeat ")]`} style={{ backgroundImage: `url(${obj.src})` }}>
-                {/* <img src={obj.src} width={'80%'} className="h-inherit" /> */}
-                {/* <img src={obj.src} className="lg:w-456 w-320" /> */}
-                {/* <img src={obj.src} className="w-2/3" /> */}
             </div>
         </div>
     )
@@ -70,15 +71,21 @@ const alignSection = (position: string) => {
 
 const animationClass = (position: string) => {
     if (position === 'left') {
-        return 'slide-left'
+        return 'animate__fadeInLeft'
     }
     if (position === 'right') {
-        return 'slide-right'
+        return 'animate__fadeInRight'
     }
 }
 
 const ServiceDetails = ({ arr }: { arr: ServiceType[] }) => {
     // const obj = ServiceJSON[0]
+
+    useEffect(() => {
+        new WOW.WOW({
+        }).init();
+
+    })
 
     return (
         <>
@@ -87,20 +94,20 @@ const ServiceDetails = ({ arr }: { arr: ServiceType[] }) => {
 
                 arr.map((serviceObj, index) => {
                     return (
-                        <div className={`section-container ${animationClass(serviceObj.image.position)} ${serviceObj.classes}`} id={serviceObj.id || "random_id"}>
-                            <div className='flex flex-col lg:flex-row flex-1 h-full'>
+                        <div className={`section-container ${serviceObj.classes}`} id={serviceObj.id || "random_id"}>
+                            <div className='flex flex-col lg:flex-row'>
 
-                                {serviceObj.image.position === 'left' && <ImageContainer obj={serviceObj.image} />}
-                                <div className='flex flex-col flex-1 text-left pl-4'>
+                                {serviceObj.image.position === 'left' && <ImageContainer obj={serviceObj.image} position='right' />}
+                                <div className={`flex flex-col flex-1 text-left pl-4 wow animate__animated  ${animationClass(serviceObj.image.position)}`}>
                                     <div className='service-title mb-5'>{serviceObj.title}</div>
                                     <div className='text-medium' >{serviceObj.description}</div>
                                     <BulletList lists={serviceObj.pointers} />
                                 </div>
-                                {serviceObj.image.position === 'right' && <ImageContainer obj={serviceObj.image} />}
+                                {serviceObj.image.position === 'right' && <ImageContainer obj={serviceObj.image} position='left' />}
 
                             </div>
 
-                            <div className={`flex w-full  ${alignSection(serviceObj.image.position)}`}>
+                            <div className={`flex w-full  wow animate__animated animate__fadeInLeft ${alignSection(serviceObj.image.position)}`}>
                                 <div className="lg:basis-1/2">
                                     <WorkTogetherSection description={serviceObj.ctaDescription} />
                                 </div>
